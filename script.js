ae2654c3e09217512f0f3c2b8931bda83fdbd8b8
PhMachado-ctrl <80326741+PhMachado-ctrl@users.noreply.github.com>
2021-04-06 13:05:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function soma() {
+    var n1=document.getElementById("num1").value;
+    var n2=document.getElementById("num2").value;
+    var soma = parseInt(n1) + parseInt(n2) ;
+    alert(soma);
+ }
+
+ function subtracao() {
+    var n1=document.getElementById("num1").value;
+    var n2=document.getElementById("num2").value;
+    var soma = parseInt(n1) - parseInt(n2);
+    2 ;
+    alert(soma);
+ } 
+
+ function multiplicacao() {
+    var n1=document.getElementById("num1").value;
+    var n2=document.getElementById("num2").value;
+    var soma =parseInt(n1) * parseInt(n2);
+    alert(soma);
+ } 
+
+ function divisao() {
+    var n1=document.getElementById("num1").value;
+    var n2=document.getElementById("num2").value;
+    var soma = parseInt(n1) / parseInt(n2) ;
+    alert(soma);
+ } 
